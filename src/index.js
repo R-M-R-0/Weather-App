d48e7a7513aa2e4f1f983c6137a09fcae9035f5b
R-M-R-0 <116025289+R-M-R-0@users.noreply.github.com>
2022-12-06 21:38:16 +0000

    Added missing ID for animated icon

--- src/index.js
+++ src/index.js
@@ -53,10 +53,16 @@ function showWeather(response) {
   let descriptionElement = document.querySelector("#weatherDescription");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   tempDisplay.innerHTML = `${temperature}`;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `assets/animated/${response.data.weather[0].description}.gif`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 // Feature 4 - when location button clicked triggers retrievePosition function
